b41bc0f0397c45b58f9b44f7b91ebdcf214805f7
Michal Vanco <michal.vanco@gooddata.com>
2019-11-07 21:25:03 +0100

    spring: add readme

--- spring-boot-template/hooks/post_gen_project.sh
+++ spring-boot-template/hooks/post_gen_project.sh
@@ -3,20 +3,17 @@
 echo "Generating project using the start.spring.io tooling"
 
 curl https://start.spring.io/starter.zip -o {{cookiecutter.app_name}}.zip \
--d 'groupId={{cookiecutter.group_id}}' \
--d 'artifactId={{cookiecutter.artifact_id}}' \
--d 'name={{cookiecutter.app_name}}' \
--d 'description={{cookiecutter.description}}' \
--d 'packageName={{cookiecutter.package_name}}' \
--d 'dependencies={{cookiecutter.dependencies}}' \
--d 'javaVersion={{cookiecutter.java_version}}' \
--d 'language={{cookiecutter.app_language}}' \
--d 'type={{cookiecutter.build_system}}-project' \
--d 'bootVersion={{cookiecutter.boot_version}}' \
--d 'packaging=jar'
+  -d 'groupId={{cookiecutter.group_id}}' \
+  -d 'artifactId={{cookiecutter.artifact_id}}' \
+  -d 'name={{cookiecutter.app_name}}' \
+  -d 'description={{cookiecutter.description}}' \
+  -d 'packageName={{cookiecutter.package_name}}' \
+  -d 'dependencies={{cookiecutter.dependencies}}' \
+  -d 'javaVersion={{cookiecutter.java_version}}' \
+  -d 'language={{cookiecutter.app_language}}' \
+  -d 'type={{cookiecutter.build_system}}-project' \
+  -d 'bootVersion={{cookiecutter.boot_version}}' \
+  -d 'packaging=jar'
 
 unzip {{cookiecutter.app_name}}.zip
-
 rm {{cookiecutter.app_name}}.zip
-
-
